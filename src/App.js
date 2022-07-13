f21b3d6f9e991aef3609395818d141ef08be712c
Alex <68344206+avilla2@users.noreply.github.com>
2022-07-13 00:49:28 -0700

    starter code

--- src/App.js
+++ src/App.js
@@ -1,56 +1,19 @@
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as React from "react";
-import {
-  Navbar,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  Collapse,
-  NavbarToggler
-} from "reactstrap";
-import logo from "./images/logo.jpeg";
-import cover from "./images/cover.jpeg";
+import CustomNavbar from "./Navbar";
+import Home from "./Home.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
-  const [isOpen, setIsOpen] = React.useState(false);
-  const toggle = () => setIsOpen(!isOpen);
-
   return (
     <div className="App">
-      <Navbar fixed="top" dark color="danger" expand="md">
-        <NavbarBrand href="/">Academia Latina</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav navbar>
-            <NavItem>
-              <NavLink href="https://inside.sou.edu/youth/latino/leadership.html">
-                Academia Leadership
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://inside.sou.edu/youth/latino/junior-counselor.html">
-                Junior Counselors
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://inside.sou.edu/youth/latino/senior-counselor.html">
-                Senior Counselors
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://inside.sou.edu/youth/latino/head-resident-information.html">
-                Head Residents
-              </NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
-      <div>
-        <img src={logo} className="logo" alt="Academia Logo" />
-        <img src={cover} className="cover" alt="Academia Cover" />
-      </div>
+      <CustomNavbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function Home() {
+  return (
+    <div className="body">
+      <h1>Welcome to Academia Latina</h1>
+    </div>
+  );
+}
